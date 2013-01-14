47f83eaea4975090b2d6573795eee66afe2a701e
Sam Phippen <samphippen@googlemail.com>
2013-01-14 21:06:06 +0000

    Make deploy.sh do something useful.
    Signed-off-by: Sam Phippen <samphippen@googlemail.com>

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-ssh tea rm -rf markov
-scp -r . tea:markov/
-echo "Restart it in screen yo!"
-ssh tea
+ssh tea "cd markov/checkout && kill -2 \`cat .andrey.pid\`"
+ssh tea rm -rf markov/checkout
+git push tea:markov/repo.git master
+ssh tea "cd markov && git clone ./repo.git checkout"
+ssh tea "cd markov/checkout && screen -d -m ruby -Ilib/ bin/andreymarkov"
