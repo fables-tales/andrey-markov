f870bf9b455aaf60f5cf33e086700016afeea359
Sam Phippen <samphippen@googlemail.com>
2013-01-14 21:29:46 +0000

    Make deploy.sh save the text
    Signed-off-by: Sam Phippen <samphippen@googlemail.com>

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+ssh tea "cp markov/checkout/.andrey /tmp"
 ssh tea "cd markov/checkout && kill -2 \`cat .andrey.pid\`"
 ssh tea rm -rf markov/checkout
 git push tea:markov/repo.git master
 ssh tea "cd markov && git clone ./repo.git checkout"
+ssh tea "cp /tmp/.andrey markov/checkout"
 ssh tea "cd markov/checkout && screen -d -m ruby -Ilib/ bin/andreymarkov"
