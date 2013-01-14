43dc66d2cb13fa4682e5c790b4cfb9a2fd739dfb
Sam Phippen <samphippen@googlemail.com>
2013-01-14 22:10:52 +0000

    Make the view data script do something useful.
    Signed-off-by: Sam Phippen <samphippen@googlemail.com>

--- view_data.rb
+++ view_data.rb
@@ -1,3 +1,13 @@
 require "pp"
 
-PP.pp(Marshal.load(File.binread(".andrey")))
+hash = Marshal.load(File.binread(".andrey"))
+outer_count = hash.keys.count
+inner_count = 0
+hash.each do |key,value|
+  value.each do |key2, value2|
+    inner_count += value2
+  end
+end
+
+puts outer_count
+puts inner_count
